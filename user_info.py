3f887b48891ef607a6af78f1287d1f7dfc2534cc
JAISHRIJ <131765983+JAISHRIJ@users.noreply.github.com>
2025-05-20 15:15:37 +0530

    Add files via upload

---
+++ user_info.py
@@ -0,0 +1,7 @@
+#Ask the user for their name,age,and city.Then print a sentence like:"Hi Arun,you are 23 years old and you live in coimbatore."
+#Ask the user for their name,age,and city.
+name=input("Enter your name:")
+age=input("Enter your age:")
+city=input("Enter your city:")
+#Print the formatted output
+print(F"Hi {name},you are {age}years old and you live in {city}.")
